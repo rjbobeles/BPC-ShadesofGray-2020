e873353ec5b21dcafe676d18542e6af8a7408883
Gel Gamoras <gelgamoras@gmail.com>
2020-10-14 13:19:48 +0800

    installed vue kinesis

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueKinesis from 'vue-kinesis'
 import App from './App.vue'
 import router from './router'
 
@@ -6,6 +7,8 @@ import './assets/scss/tailwind.scss'
 import './assets/scss/styles.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(VueKinesis)
 
 new Vue({
   router,
