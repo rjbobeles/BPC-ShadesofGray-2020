6d42930945504f31193c7d224876b294ba1be1b8
Gel Gamoras <gelgamoras@gmail.com>
2020-10-17 08:17:05 +0800

    installed laxjs

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,26 @@
 
 <script>
 import Loading from './components/Loading.vue'
+import SmoothScroll from './assets/js/smoothscroll'
 
 export default {
   components: {
     Loading,
+  },
+  methods: {
+    UpdateLax() {
+      this.$lax.update(window.scrollY)
+      window.requestAnimationFrame(this.UpdateLax)
+    },
+  },
+  mounted() {
+    if (!window.smoothScroll) {
+      window.smoothScroll = SmoothScroll(document, 180, 20)
+    }
+    window.requestAnimationFrame(this.UpdateLax)
+  },
+  created() {
+    this.$lax.setup()
   },
 }
 </script>
