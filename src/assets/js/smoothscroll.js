1aa7382f6c9b6e6fc1105e81754ac7fae255570d
Gel Gamoras <gelgamoras@gmail.com>
2020-10-17 08:21:46 +0800

    added smoothscroll

---
+++ src/assets/js/smoothscroll.js
@@ -0,0 +1,50 @@
+const SmoothScroll = (target, speed, smooth) => {
+  let t = ''
+  if (target === document) t = document.documentElement || document.body.parentNode || document.body
+  let moving = false
+  let pos = t.scrollTop
+
+  const requestFrame = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (func) {
+        window.setTimeout(func, 1000 / 50)
+      }
+    )
+  })()
+
+  function update() {
+    moving = true
+    let delta = (pos - t.scrollTop) / smooth
+
+    delta -= 1
+
+    if (pos - t.scrollTop === smooth * 2) delta = 0
+
+    t.scrollTop += delta
+    if (Math.abs(delta) > 0.5) requestFrame(update)
+    else moving = false
+  }
+
+  function scrolled(e) {
+    e.preventDefault()
+    let delta = e.delta || e.wheelDelta
+    if (delta === undefined) {
+      delta = -e.detail
+    }
+    delta = Math.max(-1, Math.min(1, delta))
+
+    pos += -delta * speed
+    pos = Math.max(0, Math.min(pos, t.scrollHeight - t.clientHeight + smooth * 2))
+
+    if (!moving) update()
+  }
+
+  target.addEventListener('mousewheel', scrolled, false)
+}
+
+export default SmoothScroll
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import VueKinesis from 'vue-kinesis'
+import lax from 'lax.js'
 import App from './App.vue'
 import router from './router'
 
@@ -8,6 +9,7 @@ import './assets/scss/tailwind.scss'
 import './assets/scss/styles.scss'
 
 Vue.config.productionTip = false
+Vue.prototype.$lax = lax
 
 Vue.use(VueKinesis)
 Vue.use(Vuelidate)
