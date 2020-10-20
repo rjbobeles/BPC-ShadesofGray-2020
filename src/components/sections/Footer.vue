324317cd8da01c2a2f47b4323a07b8a1824819a3
Gel Gamoras <gelgamoras@gmail.com>
2020-10-20 20:15:31 +0800

    added bg to header and footer

--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="footer bg-parchment">
-    <div class="container mx-auto px-5">
-      <div class="w-full flex justify-center items-center text-center">
-        <div class="end pt-48 pb-24 text-smoke text-xl">
-          This is the page of the end and the end of the page, the epilogue. The end awaits for you
-          and you await for the end.
-        </div>
+  <div class="footer text-white">
+    <div class="w-full flex justify-center items-center text-center quote bg-center bg-cover">
+      <div class="end relative py-56 text-smoke text-xl" ref="quote" data-lax-preset="eager">
+        This is the page of the end and the end of the page, the epilogue. The end awaits for you
+        and you await for the end.
       </div>
-      <div class="w-full flex flex-col sm:flex-row py-24 flex-wrap xl:flex-no-wrap justify-around">
-        <div class="w-full xl:w-1/3 pb-8 sm:pb-16 xl:pb-0 flex sm:justify-center xl:justify-start">
-          <div>
-            <img src="../../assets/images/nameplates/SOG_Logo_black.png" class="logo" />
-            <h1 class="elephant text-8xl lg:text-7xl strip mb-5 leading-none normal-case">
-              Epilogue
-            </h1>
+    </div>
+    <div class="fot w-full bg-top bg-cover">
+      <div class="container mx-auto px-5">
+        <div
+          class="w-full flex flex-col sm:flex-row py-32 flex-wrap xl:flex-no-wrap justify-around items-center"
+        >
+          <div
+            class="w-full xl:w-1/3 pb-8 sm:pb-16 xl:pb-0 flex sm:justify-center xl:justify-start xl:pr-24"
+          >
+            <img
+              src="../../assets/images/sog_cream_logo.png"
+              class="logo sm:w-1/2 lg:w-1/3 xl:w-full"
+            />
           </div>
-        </div>
-        <div class="w-full sm:w-2/5 lg:w-1/3 georgia text-sm text-smoke pb-12 sm:pb-0">
-          Shades of Gray is the official literary folio of the students of De La Salle-College of
-          Saint Benilde. In this year’s Shades of Gray, we’re inviting you to share untold stories,
-          might it be because it ended abruptly or have yet to come, through literature.
-          <br /><br /><br />
-          The Benildean Press Corps © 2020. All rights reserved.
-        </div>
-        <div class="w-full sm:w-2/5 lg:w-1/3 georgia text-sm text-smoke xl:pl-16">
-          STAY CONNECTED.
-          <br /><br />
-          Address <br />
-          2/F Br. Miguel Febres Cordero Building, Taft Campus, De La Salle-College of Saint Benilde,
-          Taft Avenue, Manila, Philippines
+          <div class="w-full sm:w-2/5 lg:w-1/3 georgia text-sm pb-12 sm:pb-0">
+            Shades of Gray is the official literary folio of the students of De La Salle-College of
+            Saint Benilde. In this year’s Shades of Gray, we’re inviting you to share untold
+            stories, might it be because it ended abruptly or have yet to come, through literature.
+            <br /><br /><br />
+            The Benildean Press Corps © 2020. All rights reserved.
+          </div>
+          <div class="w-full sm:w-2/5 lg:w-1/3 georgia text-sm xl:pl-16">
+            STAY CONNECTED.
+            <br /><br />
+            Address <br />
+            2/F Br. Miguel Febres Cordero Building, Taft Campus, De La Salle-College of Saint
+            Benilde, Taft Avenue, Manila, Philippines
+          </div>
         </div>
       </div>
     </div>
@@ -38,20 +42,28 @@
 <script>
 export default {
   name: 'Footer',
+  mounted() {
+    this.$lax.addElement(this.$refs.quote)
+  },
+  beforeDestroy() {
+    this.$lax.addElement(this.$refs.quote)
+  },
 }
 </script>
 
 <style scoped lang="scss">
 .footer {
+  .quote {
+    background-image: url(../../assets/images/SOG_WEBSITE_FOOTER.png);
+  }
   .end {
     font-family: 'Georgia', serif;
     font-weight: bold;
     width: 680px;
+    top: 75px;
   }
-  .logo {
-    width: 150px;
-    position: relative;
-    left: -10px;
+  .fot {
+    background-image: url(../../assets/images/footer-bg.png);
   }
 }
 
@@ -59,6 +71,9 @@ export default {
   .footer {
     .elephant {
       font-size: 18vw;
+    }
+    .logo {
+      width: 240px;
     }
   }
 }
--- src/components/sections/TitleCard.vue
+++ src/components/sections/TitleCard.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="title-card h-screen bg-parchment">
-    <div class="container h-full flex flex-col justify-center items-center mx-auto">
-      <img src="../../assets/images/nameplates/SOG_Logo_black.png" class="logo" />
-      <h1 class="elephant text-8xl lg:text-9xl strip mb-5 leading-none normal-case">Epilogue</h1>
-      <img src="../../assets/images/decor.png" class="decor" />
-      <p class="text-center w-3/4 xl:w-3/5 mt-24 text-smoke">
+  <div class="title-card h-screen bg-cover bg-center">
+    <div
+      class="container h-full flex flex-col justify-center items-center mx-auto relative"
+      ref="title"
+      data-lax-preset="eager-150"
+    >
+      <img
+        src="../../assets/images/sog_white_logo.png"
+        class="w-2/3 sm:w-1/2 md:w-2/5 lg:w-1/3 xl:w-1/4"
+      />
+      <p class="text-center w-3/4 xl:w-3/5 mt-8 text-white">
         And so it comes; the conclusion we did not expect unfurled at the palm of our hands. We hold
         each fragment together, trying to form a whole unfinished piece. What would have happened
         had it continued? Would it have been better if certain and guaranteed? There is still so
@@ -18,11 +23,18 @@
 <script>
 export default {
   name: 'TitleCard',
+  mounted() {
+    this.$lax.addElement(this.$refs.title)
+  },
+  beforeDestroy() {
+    this.$lax.addElement(this.$refs.title)
+  },
 }
 </script>
 
 <style scoped lang="scss">
 .title-card {
+  background-image: url('../../assets/images/SOG_WEBSITE_HEADER.png');
   .logo {
     width: 150px;
   }
@@ -31,11 +43,7 @@ export default {
   }
 }
 
-@media (max-width: 639px) {
-  .title-card {
-    h1 {
-      font-size: 17vw;
-    }
-  }
+.title-card > div {
+  top: 5%;
 }
 </style>
